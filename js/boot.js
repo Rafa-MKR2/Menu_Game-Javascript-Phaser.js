7e919d043ae63e5aa7992f7ce1bd3829828758fb
Rafael D.Carmo Costa <rafaeldocarmo2010@gmail.com>
2018-02-18 19:10:06 -0300

    Add files via upload

---
+++ js/boot.js
@@ -0,0 +1,14 @@
+
+var bootState ={
+
+	preload: function(){
+		game.load.image('progressBar','img/progressBar.png');
+		
+	},
+
+	create: function (){
+		game.state.start('load');
+	}
+
+	
+};
---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+(function(){
+	
+	var cnv = document.querySelector('canvas');
+	var ctx = cnv.getContext('2d');
+var maze =[
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+
+	];
+
+	for(var i in maze){
+	for(var j in maze[i]){
+	var tile = maze[i][j];
+	if(tile === 1){
+	var x = j * 40;
+	var y = i * 40;
+	ctx.fillRect(x,y,38,38);
+			}
+		}
+
+	}
+	
+
+	
+
+}());
